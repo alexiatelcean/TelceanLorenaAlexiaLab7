8214b8980e200b208714b9fa7aa42d5edb8781b5
LORENA-ALEXIA TELCEAN <lorena.telcean@stud.ubbcluj.ro>
2024-11-24 18:03:13 +0200

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace TelceanLorenaAlexiaLab7
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
